ebe1c76da61fdf27e6c587220d948d3f5725e6ac
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-03 22:04:28 +0200

    One more try

--- src/app/api/v1/bot/route.ts
+++ src/app/api/v1/bot/route.ts
@@ -11,7 +11,7 @@ export const POST = async (req: NextRequest, ...args: any[]) => {
 
 	bot.on('message', ctx => ctx.reply('Отримав ще одне повідомлення!'));
 
-	const handleUpdate = webhookCallback(bot, 'std/http');
+	const handleUpdate = webhookCallback(bot);
 
 	return handleUpdate(req, ...args);
 };
