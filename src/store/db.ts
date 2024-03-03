c671242f144d27ef94989813950607d631a2f553
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-03 15:10:58 +0200

    Commented unused DB code

--- src/store/db.ts
+++ src/store/db.ts
@@ -1,22 +1,22 @@
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { sql } from '@vercel/postgres';
-import * as schema from './schema/schema';
-import { subscriber } from './schema/schema';
+// import { drizzle } from 'drizzle-orm/vercel-postgres';
+// import { sql } from '@vercel/postgres';
+// import * as schema from './schema/schema';
+// import { subscriber } from './schema/schema';
 
-export const db = drizzle(sql, { schema });
-
-export const getSubscribers = async () => {
-	const selectResult = await db.select().from(subscriber);
-	console.log('Select result:', selectResult);
-	return selectResult;
-};
-
-export const getSubscribers2 = async () => {
-	return db.query.subscriber.findMany();
-};
-
-export type NewSubscription = typeof subscriber.$inferInsert;
-
-export const insertSubscription = async (subscription: NewSubscription) => {
-	return db.insert(subscriber).values(subscription).returning();
-};
+// export const db = drizzle(sql, { schema });
+//
+// export const getSubscribers = async () => {
+// 	const selectResult = await db.select().from(subscriber);
+// 	console.log('Select result:', selectResult);
+// 	return selectResult;
+// };
+//
+// export const getSubscribers2 = async () => {
+// 	return db.query.subscriber.findMany();
+// };
+//
+// export type NewSubscription = typeof subscriber.$inferInsert;
+//
+// export const insertSubscription = async (subscription: NewSubscription) => {
+// 	return db.insert(subscriber).values(subscription).returning();
+// };
--- src/store/schema/schema.ts
+++ src/store/schema/schema.ts
@@ -7,7 +7,8 @@ import {
 	bigint
 } from 'drizzle-orm/pg-core';
 
-export const roleEnum = pgEnum('role', ['regular', 'admin']);
+export const userRoleEnum = pgEnum('user_role', ['regular', 'admin']);
+export const userStatusEnum = pgEnum('user_status', ['active', 'canceled']);
 export const user = pgTable('user', <Record<string, PgColumnBuilder>>{
 	id: serial('id').primaryKey(),
 	telegramId: bigint('telegram_id', { mode: 'number' }).notNull().unique(),
@@ -15,5 +16,6 @@ export const user = pgTable('user', <Record<string, PgColumnBuilder>>{
 	firstName: text('first_name').notNull(),
 	lastName: text('last_name'),
 	chatId: bigint('chat_id', { mode: 'number' }).notNull().unique(),
-	role: roleEnum('role').default('regular').notNull()
+	role: userRoleEnum('role').default('regular').notNull(),
+	status: userStatusEnum('status').default('active').notNull()
 });
