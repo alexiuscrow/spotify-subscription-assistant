047e004625e2424b6ea33211305e54f356fb4706
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-03 21:43:05 +0200

    One more try

--- src/app/api/v1/bot/route.ts
+++ src/app/api/v1/bot/route.ts
@@ -1,12 +1,15 @@
+import { NextRequest } from 'next/server';
 import { Bot, webhookCallback } from 'grammy';
 
-const token = process.env.TELEGRAM_TOKEN;
-if (!token) throw new Error('TELEGRAM_TOKEN is unset');
+export const POST = async (req: NextRequest, ...args: any[]) => {
+	const token = process.env.TELEGRAM_TOKEN;
+	if (!token) throw new Error('TELEGRAM_TOKEN is unset');
 
-const bot = new Bot(token);
+	const bot = new Bot(token);
 
-bot.command('start', ctx => ctx.reply('Ласкаво просимо! Бот запущений.'));
+	bot.command('start', ctx => ctx.reply('Ласкаво просимо! Бот запущений.'));
 
-bot.on('message', ctx => ctx.reply('Отримав ще одне повідомлення!'));
+	bot.on('message', ctx => ctx.reply('Отримав ще одне повідомлення!'));
 
-export default webhookCallback(bot, 'next-js');
+	return await webhookCallback(bot, 'next-js')(req, ...args);
+};
