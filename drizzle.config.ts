3c768e97075bac1130868aedb8803b8a89e80217
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-03 15:14:16 +0200

    Updated drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { Config, defineConfig } from 'drizzle-kit';
 import '@/store/config';
 
 export default defineConfig({
-	schema: '@/store/schema/schema.ts',
+	schema: './src/store/schema/schema.ts',
 	out: './drizzle',
 	driver: 'pg',
 	dbCredentials: {
