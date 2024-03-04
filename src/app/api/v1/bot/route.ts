eed1af580853780ca6296895db864c45b4825299
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-04 13:18:02 +0200

    Tried to optimize

--- src/app/api/v1/bot/route.ts
+++ src/app/api/v1/bot/route.ts
@@ -1,25 +1,20 @@
-import { NextRequest } from 'next/server';
 import { Bot, webhookCallback } from 'grammy';
 import { type ReactionTypeEmoji } from '@grammyjs/types';
 
-export const POST = async (req: NextRequest, ...args: any[]) => {
-	const token = process.env.TELEGRAM_TOKEN;
-	if (!token) throw new Error('TELEGRAM_TOKEN is unset');
+const token = process.env.TELEGRAM_TOKEN;
+if (!token) throw new Error('TELEGRAM_TOKEN is unset');
 
-	const bot = new Bot(token);
+const bot = new Bot(token);
 
-	bot.command('start', ctx => ctx.reply('Ласкаво просимо! Бот запущений.'));
+bot.command('start', ctx => ctx.reply('Ласкаво просимо! Бот запущений.'));
 
-	bot.on('message', async ctx => {
-		const emoji = emojis[Math.floor(Math.random() * emojis.length)];
-		await ctx.react(emoji);
-		return ctx.reply('Отримав ще одне повідомлення!');
-	});
+bot.on('message', async ctx => {
+	const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+	await ctx.react(emoji);
+	return ctx.reply('Отримав ще одне повідомлення!');
+});
 
-	const handleUpdate = webhookCallback(bot, 'std/http', 'throw', 10000);
-
-	return handleUpdate(req, ...args);
-};
+export const POST = webhookCallback(bot, 'std/http');
 
 const emojis: ReactionTypeEmoji['emoji'][] = [
 	'👍',
