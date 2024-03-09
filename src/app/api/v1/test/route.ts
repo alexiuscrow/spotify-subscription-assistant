e132d946cf771f609668fed8b996b701fcbb08db
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-09 13:22:20 +0200

    Testing authenticator

--- src/app/api/v1/test/route.ts
+++ src/app/api/v1/test/route.ts
@@ -1,7 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { and, eq, exists, notExists, sql } from 'drizzle-orm';
+import { allowedUserCriteria, user } from '@/store/schema';
+import { db } from '@/store/db';
+import { PgDialect } from 'drizzle-orm/pg-core';
 
 export const dynamic = 'force-dynamic';
 
 export async function POST(request: NextRequest) {
-	return NextResponse.json({});
+	const firstName = (await request.json()).firstName || '';
+	const pgDialect = new PgDialect();
+	const data = pgDialect.sqlToQuery(
+		sql<boolean>`select ${and(
+			exists(
+				db
+					.select({ id: allowedUserCriteria.id })
+					.from(allowedUserCriteria)
+					.where(eq(allowedUserCriteria.firstName, firstName))
+			),
+			notExists(db.select({ id: user.id }).from(user).where(eq(user.firstName, firstName)))
+		)}`
+	).sql;
+	return NextResponse.json({ data });
 }
--- src/bot/authenticator.ts
+++ src/bot/authenticator.ts
@@ -1,13 +1,31 @@
 import { Middleware } from 'grammy';
-import { inspect } from 'node:util';
+import { db } from '@/store/db';
+import { allowedUserCriteria, user } from '@/store/schema';
+import { and, eq, exists, notExists, sql } from 'drizzle-orm';
 
 const authenticator: Middleware = async (ctx, next) => {
-	console.log(inspect(ctx.message));
+	if (!ctx.from) {
+		await ctx.reply('Вибачте, але я не можу знайти ваш профіль.');
+		return;
+	}
 
-	await ctx.reply(`
-		User ID: ${ctx.from?.id},
-		First Name: ${ctx.from?.first_name},
-	`);
+	const currentTelegramUser = ctx.from;
+	const storedUser = await db.query.user.findFirst({ where: eq(user.telegramId, currentTelegramUser.id) });
+
+	if (!storedUser) {
+		const firstName = currentTelegramUser.first_name;
+		const meetsCriteria = await sql<boolean>`select ${and(
+			exists(
+				db
+					.select({ id: allowedUserCriteria.id })
+					.from(allowedUserCriteria)
+					.where(eq(allowedUserCriteria.firstName, firstName))
+			),
+			notExists(db.select({ id: user.id }).from(user).where(eq(user.firstName, firstName)))
+		)}`;
+
+		await ctx.reply(meetsCriteria.toString());
+	}
 
 	await next();
 };
