b09187b01d7975312eaa88a371fcd95d4bbde0c5
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-09 23:47:10 +0200

    Added invoices command

--- src/bot/command/index.ts
+++ src/bot/command/index.ts
@@ -1,10 +1,12 @@
 import { Composer, Context } from 'grammy';
 import startCommand from '@/bot/command/start';
 import helpCommand from '@/bot/command/help';
+import invoicesCommand from '@/bot/command/invoices';
 
 const commandComposer = new Composer<Context>();
 
 commandComposer.command('start', startCommand);
 commandComposer.command('help', helpCommand);
+commandComposer.command('invoices', invoicesCommand);
 
 export default commandComposer;
