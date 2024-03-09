2986a13d9bd6682bd2d3c8eac0a784f62e9a9d16
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-09 15:45:02 +0200

    Testing authenticator

--- src/bot/command/start.ts
+++ src/bot/command/start.ts
@@ -1,12 +1,7 @@
 import { Middleware } from 'grammy';
 
 const startCommand: Middleware = async ctx => {
-	await ctx.api.setMyCommands([
-		{ command: 'help', description: 'Показати довідку' },
-		{ command: 'start', description: 'Почати роботу' }
-	]);
-
-	await ctx.reply('Привіт! Напиши /help, щоб побачити список команд.');
+	await ctx.api.setMyCommands([{ command: 'help', description: 'Показати довідку' }]);
 };
 
 export default startCommand;
