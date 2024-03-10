7e3dc19d65fb70ad023e6e4c94b05931d48ded75
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-10 20:15:28 +0200

    Trying invoice pagination menu

--- src/bot/menu/invoicePagination.ts
+++ src/bot/menu/invoicePagination.ts
@@ -2,19 +2,19 @@ import { Menu } from '@grammyjs/menu';
 import BotContext from '@/bot/BotContext';
 import invoicesCommand from '@/bot/command/invoices';
 
-const invoiceMenu = new Menu<BotContext>('invoice-pagination').dynamic((dynamicCtx, range) => {
-	const pagination = dynamicCtx.invoice?.pagination;
+const invoiceMenu = new Menu<BotContext>('invoice-pagination').dynamic((ctx, range) => {
+	const pagination = ctx.invoice?.pagination;
 	if (!!pagination) {
 		if (pagination.hasPrev) {
 			pagination.page = pagination.page - 1;
-			range.text('⬅️ Попередні', (ctx, next) =>
-				invoicesCommand({ ...dynamicCtx, ...ctx, invoice: { ...dynamicCtx.invoice, pagination } } as BotContext, next)
+			range.text('⬅️ Попередні', (ctx: BotContext, next) =>
+				invoicesCommand({ ...ctx, invoice: { ...ctx.invoice, pagination } } as BotContext, next)
 			);
 		}
 		if (pagination.hasNext) {
 			pagination.page = pagination.page + 1;
-			range.text('Наступні ➡️', (ctx, next) =>
-				invoicesCommand({ ...dynamicCtx, ...ctx, invoice: { ...dynamicCtx.invoice, pagination } } as BotContext, next)
+			range.text('Наступні ➡️', (ctx: BotContext, next) =>
+				invoicesCommand({ ...ctx, invoice: { ...ctx.invoice, pagination } } as BotContext, next)
 			);
 		}
 	}
