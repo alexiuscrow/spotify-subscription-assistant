4713ea2170c6a237f467d41ebd0038b2d8e936e1
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-19 23:45:45 +0200

    Try

--- src/store/repositories/userRepo.ts
+++ src/store/repositories/userRepo.ts
@@ -47,7 +47,7 @@ export const createUser = async (user: User) => {
 			username: user.username,
 			role: isAdmin ? 'admin' : 'regular'
 		} as NewUser)
-		.returning() as Promise<InferUser>;
+		.returning() as unknown as InferUser;
 };
 
 export const updateUser = async (id: number, values: PgUpdateSetSource<typeof userSchema>) => {
