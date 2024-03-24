728fcf794f581660e2d57f942c1c7761ab5019c5
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-24 13:26:05 +0200

    Try

--- src/bot/menu/index.ts
+++ src/bot/menu/index.ts
@@ -1,9 +1,11 @@
 import { Composer } from 'grammy';
 import invoicePagination from '@/bot/menu/invoicePagination';
+import debtPagination from '@/bot/menu/debtPagination';
 import BotContext from '@/bot/BotContext';
 
 const menuComposer = new Composer<BotContext>();
 
 menuComposer.use(invoicePagination);
+menuComposer.use(debtPagination);
 
 export default menuComposer;
