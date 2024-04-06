14e9844dcdf55e9ae402624fb0e129fe43724274
Alexey Sapon <asapon@freeconferencecall.com>
2024-04-07 00:38:27 +0300

    Revert "Revert "Try""
    This reverts commit a8573222e169c33041e262cbece3071799f333c8.

--- src/app/api/v1/test/route.ts
+++ src/app/api/v1/test/route.ts
@@ -1,13 +1,16 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import SubscriptionManager from '@/manager/SubscriptionManager';
+import SpreadsheetManager from '@/manager/SpreadsheetManager';
+import SubscriberManager from '@/manager/SubscriberManager';
 
 export const dynamic = 'force-dynamic';
 
-export async function GET() {
-	// const { searchParams } = new URL(request.url);
-	// const pos = searchParams.has('pos') ? Number(searchParams.get('pos')) : 0;
-	// let response = await getPaymentsForAllYearsBySubscriber(pos);
-	const response = await SubscriptionManager.getSubscription({ with: { subscriberHistory: true } });
+export async function GET(request: NextRequest) {
+	const { searchParams } = new URL(request.url);
+	const num = searchParams.has('num') ? Number(searchParams.get('num')) : 0;
+	const subscriber = await SubscriberManager.getSubscriberById(num, {
+		with: { user: true }
+	});
 
-	return NextResponse.json({ response });
+	return NextResponse.json({ subscriber });
 }
