524fb46a7a68ddda4db33d5c6435bea037b51e0a
Alexey Sapon <asapon@freeconferencecall.com>
2024-04-07 13:00:03 +0300

    Updated bot route

--- src/app/api/v1/bot/route.ts
+++ src/app/api/v1/bot/route.ts
@@ -2,7 +2,7 @@ import { webhookCallback } from 'grammy';
 import { getBot } from '@/bot';
 import { NextRequest } from 'next/server';
 
-export const POST = async (req: NextRequest, ...args: any[]) => {
+export const POST = async (req: NextRequest) => {
 	const bot = await getBot();
-	return webhookCallback(bot, 'std/http')(req, ...args);
+	return webhookCallback(bot, 'std/http')(req);
 };
