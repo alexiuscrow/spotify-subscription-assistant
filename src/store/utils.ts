02fa01e6c9864dcc36f75f656c9709838a2d5a13
Alexey Sapon <asapon@freeconferencecall.com>
2024-04-14 21:45:27 +0300

    Fake changes

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -1,6 +1,13 @@
 import { PgColumn, PgSelect, pgTableCreator, unique as drizzleUnique } from 'drizzle-orm/pg-core';
 import { SQL } from 'drizzle-orm';
 
+const projectPrefix = 'ssa';
+const isProduction = process.env.NODE_ENV === 'production';
+const env = isProduction ? 'prod' : 'dev';
+
+export const pgTable = pgTableCreator(name => `${projectPrefix}_${env}_${name}`);
+export const unique = (name: string) => drizzleUnique(`${projectPrefix}_${env}_${name}`);
+
 export const withPagination = <T extends PgSelect>(
 	qb: T,
 	limit = 5,
@@ -11,10 +18,3 @@ export const withPagination = <T extends PgSelect>(
 		.orderBy(...orderByColumns)
 		.limit(limit)
 		.offset((page - 1) * limit);
-
-const projectPrefix = 'ssa';
-const isProduction = process.env.NODE_ENV === 'production';
-const env = isProduction ? 'prod' : 'dev';
-
-export const pgTable = pgTableCreator(name => `${projectPrefix}_${env}_${name}`);
-export const unique = (name: string) => drizzleUnique(`${projectPrefix}_${env}_${name}`);
