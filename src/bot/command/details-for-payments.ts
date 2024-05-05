0e69093c93354f84b118f7e2375ac665ff6825d1
Alexey Sapon <asapon@freeconferencecall.com>
2024-05-05 22:49:34 +0300

    Fix localization reference in payment details
    An extra space was removed on the 'bank-name' localization reference in the 'details-for-payments' command. This addresses an issue where payment credit card details weren't accurately shown due to faulty data formatting.

--- src/bot/command/details-for-payments.ts
+++ src/bot/command/details-for-payments.ts
@@ -7,7 +7,7 @@ const detailsForPaymentsCommand: Middleware<BotContext> = async ctx => {
 		markdownv2.bold(ctx.t('details-for-payments')),
 		'',
 		ctx.t('card-number-caption'),
-		`${process.env.MONOBANK_PAYMENT_CREDIT_CARD} ${markdownv2.escape(`(${ctx.t('bank-name ')})`)}`,
+		`${process.env.MONOBANK_PAYMENT_CREDIT_CARD} ${markdownv2.escape(`(${ctx.t('bank-name')})`)}`,
 		'',
 		ctx.t('payment-link-caption'),
 		markdownv2.escape(process.env.MONOBANK_PAYMENT_LINK as string)
