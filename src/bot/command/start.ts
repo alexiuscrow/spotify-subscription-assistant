a5abead5bc3324577749a1878acc37279a807181
Alexey Sapon <asapon@freeconferencecall.com>
2024-05-05 19:25:59 +0300

    Add 'my_status' command to bot for all users
    Previously, the 'my_status' command was only accessible for non-admin users. Now, it has been added as a general command accessible to all users. The condition to add it only for non-admin users has been removed.

--- src/bot/command/start.ts
+++ src/bot/command/start.ts
@@ -4,15 +4,10 @@ import { BotCommand } from '@grammyjs/types';
 
 const startCommand: Middleware<BotContext> = async ctx => {
 	const commands: BotCommand[] = [
+		{ command: 'my_status', description: 'Мої платежі' },
 		{ command: 'invoices', description: 'Списання за підписку' },
 		{ command: 'details_for_payments', description: 'Реквізити для платежів' }
 	];
-
-	if (ctx.session.user?.role === 'admin') {
-		commands.unshift({ command: 'debtors', description: 'Дебітори' });
-	} else {
-		commands.unshift({ command: 'my_status', description: 'Мої платежі' });
-	}
 
 	await ctx.api.setMyCommands(commands);
 };
