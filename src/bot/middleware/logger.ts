b9ef3464dff3ca57af0455339c2fc1d0c20c0625
Alexey Sapon <asapon@freeconferencecall.com>
2024-05-11 21:58:21 +0300

    Remove redundant logging statement
    Removed an unnecessary logging statement in the logger middleware for the bot. This change simplifies the logging process, eliminating superfluous logging which was causing noise in the logs.

--- src/bot/middleware/logger.ts
+++ src/bot/middleware/logger.ts
@@ -4,7 +4,6 @@ import { inspect } from 'node:util';
 import logger from '@/logger';
 
 const loggerMiddleware: Middleware<BotContext> = async (ctx, next) => {
-	logger.info('Got new request..');
 	logger.info(inspect(ctx, { depth: null }));
 
 	await next();
