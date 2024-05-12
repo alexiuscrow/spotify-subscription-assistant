85b2c761a2e124838a7668d683ca7f3c50d0d990
Alexey Sapon <asapon@freeconferencecall.com>
2024-05-12 13:04:54 +0300

    Refactor invoices command text in bot middleware
    The invoices command text in the bot's middleware has been simplified. The extra code used to add the command to the chat scope has also been removed. The simplification retains the previous function while eliminating extraneous formatting and usage.

--- src/bot/middleware/commandsDescriptionMiddleware.ts
+++ src/bot/middleware/commandsDescriptionMiddleware.ts
@@ -1,5 +1,3 @@
-// noinspection TypeScriptValidateTypes
-
 import { Middleware } from 'grammy';
 import BotContext from '@/bot/BotContext';
 import { Commands } from '@grammyjs/commands';
@@ -7,10 +5,7 @@ import { Commands } from '@grammyjs/commands';
 const commandsDescriptionMiddleware: Middleware<BotContext> = async (ctx, next) => {
 	const cmds = new Commands();
 
-	cmds
-		.command('invoices', 'Списання за підписку')
-		.localize('en', 'invoices', 'Subscription charges')
-		.addToScope({ type: 'all_private_chats' }, ctx => ctx.reply('ok'));
+	cmds.command('invoices', 'Списання за підписку!!!').localize('en', 'invoices', 'Subscription charges!!!');
 
 	await ctx.setMyCommands(cmds);
 	await next();
