5d0163462808a3028923cb9f90c57f9e398d37b6
Alexey Sapon <asapon@freeconferencecall.com>
2024-05-12 12:55:45 +0300

    Update commands middleware configuration
    Improved the configuration of Commands middleware in Telegram bot by specifying generic type and adding the command to all private chats. The generic type for Commands is set to BotContext for better type-safety. The 'invoices' command has been extended to respond in all private chats for better accessibility.

--- src/bot/middleware/commandsDescriptionMiddleware.ts
+++ src/bot/middleware/commandsDescriptionMiddleware.ts
@@ -1,11 +1,16 @@
+// noinspection TypeScriptValidateTypes
+
 import { Middleware } from 'grammy';
 import BotContext from '@/bot/BotContext';
 import { Commands } from '@grammyjs/commands';
 
 const commandsDescriptionMiddleware: Middleware<BotContext> = async (ctx, next) => {
-	const cmds = new Commands();
+	const cmds = new Commands<BotContext>();
 
-	cmds.command('invoices', 'Списання за підписку').localize('en', 'invoices', 'Subscription charges');
+	cmds
+		.command('invoices', 'Списання за підписку')
+		.localize('en', 'invoices', 'Subscription charges')
+		.addToScope({ type: 'all_private_chats' }, ctx => ctx.reply('ok'));
 
 	await ctx.setMyCommands(cmds);
 	await next();
