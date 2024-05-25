a21db55b8dbe89e6afc373dc205eb399cb8c99dd
Alexey Sapon <asapon@freeconferencecall.com>
2024-05-25 18:01:05 +0300

    Added 'start' command to bot
    A new command named 'start' has been added to the command bot. This command guides users on the commands available for use. A new file - start.ts was created to host the implementation details of the 'start' command.

--- src/bot/command/index.ts
+++ src/bot/command/index.ts
@@ -4,9 +4,11 @@ import invoicesCommand from '@/bot/command/invoices';
 import myStatusCommand from '@/bot/command/my-status';
 import BotContext from '@/bot/BotContext';
 import debtorsCommand from '@/bot/command/debtors';
+import startCommand from '@/bot/command/start';
 
 const commandComposer = new Composer<BotContext>();
 
+commandComposer.command('start', startCommand);
 commandComposer.command('my_status', myStatusCommand);
 commandComposer.command('details_for_payments', detailsForPaymentsCommand);
 commandComposer.command('invoices', invoicesCommand);
---
+++ src/bot/command/start.ts
@@ -0,0 +1,21 @@
+import { Middleware } from 'grammy';
+import BotContext from '@/bot/BotContext';
+import { markdownv2 } from 'telegram-format';
+
+const startCommand: Middleware<BotContext> = async ctx => {
+	const outputLines = [
+		markdownv2.bold('Вам наступні наступні команди:'),
+		'',
+		'/my_status - Стан ваших платежів та заборгованності',
+		'',
+		'/invoices - Списання за підписку Spotify Premium',
+		'',
+		'/details_for_payments - Можливі способи оплати'
+	];
+
+	await ctx.reply(outputLines.join('\n'), {
+		parse_mode: 'MarkdownV2'
+	});
+};
+
+export default startCommand;
